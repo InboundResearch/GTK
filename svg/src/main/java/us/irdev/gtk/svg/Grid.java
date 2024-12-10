08770fd95bf47804f0a41b024d73f5fa398f2f42
Bretton Wade <bretton.wade@gmail.com>
2024-12-10 17:55:33 -0500

    feature/1.0.3-jurisdiction - tweaks to grid, removing built files

--- svg/src/main/java/us/irdev/gtk/svg/Grid.java
+++ svg/src/main/java/us/irdev/gtk/svg/Grid.java
@@ -4,32 +4,60 @@ import us.irdev.gtk.xyw.Domain;
 import us.irdev.gtk.xyw.Tuple;
 
 import static us.irdev.gtk.xyw.Tuple.PT;
+import static us.irdev.gtk.xyw.Tuple.VEC;
 
 public class Grid implements Element {
-  private int xSteps, ySteps;
+  private static final int DEFAULT_STEP_COUNT = 10;
+  private final int defaultStepCount;
+  private final Tuple spacing;
+  private final Tuple origin;
 
-  public Grid (int xSteps, int ySteps) {
-    this.xSteps = xSteps; this.ySteps = ySteps;
+  public Grid (Tuple spacing, Tuple origin, int defaultStepCount) {
+    // condition the input, default to whatever spacing was passed in for spacing, and fix the
+    // origin if it wasn't supplied
+    this.spacing = spacing;
+    this.origin = (origin != null) ? origin : Tuple.ORIGIN;
+    this.defaultStepCount = defaultStepCount;
+  }
+
+  public Grid (Tuple spacing, Tuple origin) {
+    this(spacing, origin, DEFAULT_STEP_COUNT);
+  }
+
+  public Grid (Tuple spacing) {
+    this(spacing, null, DEFAULT_STEP_COUNT);
+  }
+
+  public Grid (int defaultStepCount) {
+    this(null, null, defaultStepCount);
+  }
+
+  public Grid () {
+    this(null, null, DEFAULT_STEP_COUNT);
   }
 
   @Override
   public String emit (Domain domain, Traits traits) {
+    // condition the spacing, if the result should be dynamically computed
+    Tuple spacing = (this.spacing != null) ? this.spacing : domain.size().scale (1.0 / defaultStepCount);
+
     // emit gridlines using the current traits
     StringBuilder builder = new StringBuilder();
 
+    // compute the size of the domain, the bottom left of the grid, and the number of steps
     Tuple size = domain.size();
+    Tuple bottomLeft = domain.min.hquotient (spacing).floor().hproduct (spacing);
+    Tuple stepCount = size.hquotient (spacing).ceil();
 
-    // emit the vertical axis lines
-    double spacing = size.x / xSteps;
-    for (int i = 0; i <= xSteps; ++i) {
-      double x = domain.min.x + (i * spacing);
+    // emit the vertical axis lines, starting with the first grid line >= left of the domain
+    for (int i = 0; i <= (int) stepCount.x; ++i) {
+      double x = bottomLeft.x + (i * spacing.x);
       builder.append (new Line (PT (x, domain.min.y), PT(x, domain.max.y)).emit (domain, traits));
     }
 
     // emit the horizontal axis lines
-    spacing = size.y / ySteps;
-    for (int i = 0; i <= ySteps; ++i) {
-      double y = domain.min.y + (i * spacing);
+    for (int i = 0; i <= (int) stepCount.y; ++i) {
+      double y = bottomLeft.y + (i * spacing.y);
       builder.append (new Line (PT (domain.min.x, y), PT(domain.max.x, y)).emit (domain, traits));
     }
 
